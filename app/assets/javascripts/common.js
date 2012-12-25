9ce4e1073922ea3dec435440602001700bdb9cbd
chetan-redkivi <cheyan.tomar@redkivi.com>
2012-12-25 16:49:07 +0530

    final touch

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
     $('.message').click(function(){
         $('.message').fadeOut(1000);
     })
-
+    $(function() {
+        setTimeout(function() {
+            $(".message").hide('blind', {}, 500)
+         }, 3000);
+    });
 });
