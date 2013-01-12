c7aeec9b283569e6a0db61ecf6b6905ea544cd0e
chetan-redkivi <cheyan.tomar@redkivi.com>
2013-01-12 23:42:25 +0530

    Removing API key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -208,7 +208,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   user_scopes = "user_birthday,user_education_history,user_likes,user_location,user_relationships,user_status,user_religion_politics,user_photos,user_groups"
   friends_scope = "publish_stream, offline_access,friends_birthday,email,friends_education_history,friends_location,friends_relationships,friends_status,friends_religion_politics"
-  config.omniauth :facebook, "206656552803930", "7d5737d80a56961ac38907dd104730ec",{:scope => "#{user_scopes},#{friends_scope}", :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  config.omniauth :facebook, ENV['APP_KEY'], ENV['APP_SECRET'],{:scope => "#{user_scopes},#{friends_scope}", :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
